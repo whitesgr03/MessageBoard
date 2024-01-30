37bad3c5d48e57b908fd6357ac909500f4eb6f7a
Weiss Bai <whitesgr03@gmail.com>
2024-01-30 12:51:43 +0800

    Add router get and post

--- routes/message.js
+++ routes/message.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {});
+router.get("/", (req, res) => {
+	res.render("form");
+});
+
+router.post("/", (req, res) => {
+	console.log(req.body);
+	res.redirect("/new");
+});
 
 module.exports = router;
