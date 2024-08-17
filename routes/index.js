4b7d638b7e250c55160206c981eec91c49b1b825
Weiss Bai <whitesgr03@gmail.com>
2024-08-17 10:23:44 +0800

    Refactor index router
    Delete all routes.
    
    Add route to redirect to path "/messages".

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,7 @@
-const express = require("express");
+import express from "express";
+
 const router = express.Router();
 
-const messages = [
-	{
-		text: "Hi there!",
-		user: "Amando",
-		added: new Date(),
-	},
-	{
-		text: "Hello World!",
-		user: "Charles",
-		added: new Date(),
-	},
-];
+router.get("/", (req, res) => res.redirect("/messages"));
 
-router.get("/", (req, res) => {
-	res.render("index", {
-		title: "Mini Messageboard",
-		messages: messages,
-	});
-});
-
-router.post("/new", (req, res) => {
-	const { author, message } = req.body;
-
-	const newMessage = {
-		text: message,
-		user: author,
-		added: new Date(),
-	};
-	messages.push(newMessage);
-
-	res.redirect("/");
-});
-
-module.exports = router;
+export default router;
