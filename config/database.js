64362232f1c4227a4a551e11107516c0a4b30f10
Weiss Bai <whitesgr03@gmail.com>
2024-08-19 00:02:10 +0800

    Update database.js
    Rename pool to db variable.
    
    Delete initialTables function.

--- config/database.js
+++ config/database.js
@@ -5,58 +5,17 @@ const databaseLog = debug("PostgreSQL");
 const { Pool } = pg;
 
 databaseLog("Connecting PostgreSQL...");
-const pool = new Pool({
+const db = new Pool({
 	connectionString: process.env.DATABASE_URL,
-});
-pool.on("error", async err => {
-	databaseLog("Connect error:");
-	databaseLog(err);
-	databaseLog(`Database is closed.`);
-	await pool.end();
-	process.exit(1);
 });
 databaseLog("PostgreSQL connect successful");
 
-const initialTables = async () => {
-	const checkTable = `SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name= $1) AS exist;`;
-	const tables = {
-		messages: `CREATE TABLE messages (
-	                	id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	                    content VARCHAR ( 100 ) NOT NULL,
-	                    username VARCHAR ( 30 ) NOT NULL,
-	                    created_at timestamptz NOT NULL
-	                );`,
-	};
-	const populate = {
-		messages: {
-			text: `INSERT INTO messages (content, username, created_at)
-						VALUES
-							('Hi there!', 'Amando', $1),
-							('Hello World!', 'Jackson', $1)
-					;`,
-			values: [new Date()],
-		},
-	};
+db.on("error", async err => {
+	databaseLog("Connect error:");
+	databaseLog(err);
+	databaseLog(`Database is closed.`);
+	await db.end();
+	process.exit(1);
+});
 
-	databaseLog("Creating tables...");
-	await Promise.all(
-		Object.keys(tables).map(async name => {
-			const { rows } = await pool.query(checkTable, [name]);
-			return (
-				!rows[0].exist &&
-				pool
-					.query(tables[name])
-					.then(
-						async () =>
-							await pool.query(
-								populate[name].text,
-								populate[name].values
-							)
-					)
-			);
-		})
-	);
-	databaseLog("The tables were created.");
-};
-
-export { pool, initialTables };
+export default db;
